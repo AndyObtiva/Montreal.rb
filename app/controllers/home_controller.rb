1786f78f675ddb1d8fc2806c38ed8d83ba73cb4a
Benjamin Thouret <ben@2ret.com>
2015-12-04 07:39:03 -0500

    Activate markdown renderer and limit news_items to 10 on homepage
    TODO: create a pagination ticket

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,8 @@ class HomeController < ApplicationController
 
   def index
     @events = Event.published.limit(2)
-    @news_items = NewsItem.published.to_a
+    # TODO: Pagination
+    @news_items = NewsItem.published.limit(10).to_a
   end
 
 end
