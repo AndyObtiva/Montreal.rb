5bcabc09e8aaa7271cf493fcb942ed521b309c1d
Benjamin Thouret <ben@2ret.com>
2015-12-04 08:21:59 -0500

    Remove TODO comment

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,6 @@ class HomeController < ApplicationController
 
   def index
     @events = Event.published.limit(2)
-    # TODO: Pagination
     @news_items = NewsItem.published.limit(10).to_a
   end
 
